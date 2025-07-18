23704aa73f2e67f896e36bcd8bc595fa9adab2e9
joy-dey <joy.dey@outlook.com>
2025-07-18 17:52:24 +0530

    feat(chat-widget): add allowNotifications prop and refactor clearMessage implementation

--- src/components/chat-widget/chat-widget.tsx
+++ src/components/chat-widget/chat-widget.tsx
@@ -16,6 +16,7 @@ export class ChatWidget {
   @Prop() buttonBackground: string = 'oklch(0.491 0.27 292.581)';
   @Prop() maxReconnectAttempts = 5;
   @Prop() disclaimerText = "I'm an AI chatbot. While I aim for accuracy, my responses may not always be entirely correct or up-to-date.";
+  @Prop() allowNotifications: boolean = false;
 
   @State() messages: ChatMessage[] = chatState.messages;
   @State() isOpen: boolean = chatState.isOpen;
@@ -40,7 +41,6 @@ export class ChatWidget {
   }
 
   componentWillLoad() {
-    chatActions.clearMessages();
     if (this.socketURL) {
       this.connectWebSocket();
     } else {
@@ -55,6 +55,7 @@ export class ChatWidget {
   }
 
   private connectWebSocket() {
+    chatActions.clearMessages();
     if (!this.socketURL || this.socketURL.trim() === '') return;
 
     this.socket = new WebSocket(this.socketURL);
@@ -72,7 +73,11 @@ export class ChatWidget {
     this.socket.onmessage = event => {
       chatActions.setBotTyping(false);
       clearTimeout(this.responseTimeout);
-      this.notify(event.data);
+
+      if (this.allowNotifications) {
+        this.notify(event.data);
+      }
+
       const incomingMessage: ChatMessage = { type: 'bot', message: event.data, timestamp: this.formatDateIntl(new Date()) };
       chatActions.addMessage(incomingMessage);
     };
@@ -177,11 +182,13 @@ export class ChatWidget {
             }}
           ></chat-area>
         )}
-        <button class="juno-fab" style={{ background: this.buttonBackground }} onClick={() => this.handleClick()}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
-            <path d="M20.7134 8.12811L20.4668 8.69379C20.2864 9.10792 19.7136 9.10792 19.5331 8.69379L19.2866 8.12811C18.8471 7.11947 18.0555 6.31641 17.0677 5.87708L16.308 5.53922C15.8973 5.35653 15.8973 4.75881 16.308 4.57612L17.0252 4.25714C18.0384 3.80651 18.8442 2.97373 19.2761 1.93083L19.5293 1.31953C19.7058 0.893489 20.2942 0.893489 20.4706 1.31953L20.7238 1.93083C21.1558 2.97373 21.9616 3.80651 22.9748 4.25714L23.6919 4.57612C24.1027 4.75881 24.1027 5.35653 23.6919 5.53922L22.9323 5.87708C21.9445 6.31641 21.1529 7.11947 20.7134 8.12811ZM20 11C20.6986 11 21.3694 10.8806 21.9929 10.6611C21.9976 10.7735 22 10.8865 22 11C22 15.4183 18.4183 19 14 19V22.5C9 20.5 2 17.5 2 11C2 6.58172 5.58172 3 10 3H14C14.1135 3 14.2265 3.00237 14.3389 3.00705C14.1194 3.63061 14 4.30136 14 5C14 8.31371 16.6863 11 20 11Z"></path>
-          </svg>
-        </button>
+        {!this.isOpen && (
+          <button class="juno-fab" style={{ background: this.buttonBackground }} onClick={() => this.handleClick()}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor">
+              <path d="M20.7134 8.12811L20.4668 8.69379C20.2864 9.10792 19.7136 9.10792 19.5331 8.69379L19.2866 8.12811C18.8471 7.11947 18.0555 6.31641 17.0677 5.87708L16.308 5.53922C15.8973 5.35653 15.8973 4.75881 16.308 4.57612L17.0252 4.25714C18.0384 3.80651 18.8442 2.97373 19.2761 1.93083L19.5293 1.31953C19.7058 0.893489 20.2942 0.893489 20.4706 1.31953L20.7238 1.93083C21.1558 2.97373 21.9616 3.80651 22.9748 4.25714L23.6919 4.57612C24.1027 4.75881 24.1027 5.35653 23.6919 5.53922L22.9323 5.87708C21.9445 6.31641 21.1529 7.11947 20.7134 8.12811ZM20 11C20.6986 11 21.3694 10.8806 21.9929 10.6611C21.9976 10.7735 22 10.8865 22 11C22 15.4183 18.4183 19 14 19V22.5C9 20.5 2 17.5 2 11C2 6.58172 5.58172 3 10 3H14C14.1135 3 14.2265 3.00237 14.3389 3.00705C14.1194 3.63061 14 4.30136 14 5C14 8.31371 16.6863 11 20 11Z"></path>
+            </svg>
+          </button>
+        )}
       </Host>
     );
   }
